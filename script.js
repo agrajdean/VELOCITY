7f9594a157ff8dd297d0d681db38bd74d436c2b6
Andrei Grajdean <101287269+agrajdean@users.noreply.github.com>
2023-12-26 19:47:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+// TABS
+const tabsBtns = document.querySelectorAll(".tabs__nav button");
+const tabsItems = document.querySelectorAll(".tabs__item");
+
+// Фун-ция скрывает табы и убирает active у кнопок.
+
+function hideTabs() {
+  tabsItems.forEach((item) => item.classList.add("hide"));
+  tabsBtns.forEach((item) => item.classList.remove("active"));
+}
+
+// Фун-ция показывает переданный № таба и делает соответ-щую ему кнопку активной.
+
+function showTab(index) {
+  tabsItems[index].classList.remove("hide");
+  tabsBtns[index].classList.add("active");
+}
+
+hideTabs();
+showTab(0);
+
+tabsBtns.forEach((btn, index) =>
+  btn.addEventListener("click", () => {
+    hideTabs();
+    showTab(index);
+  })
+);
+
+// ANCHORS
+
+const anchors = document.querySelectorAll(".header__nav a");
+
+anchors.forEach((anc) => {
+  anc.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const id = anc.getAttribute("href");
+    const elem = document.querySelector(id);
+
+    window.scroll({
+      top: elem.offsetTop - 75,
+      behavior: "smooth",
+    });
+  });
+});
